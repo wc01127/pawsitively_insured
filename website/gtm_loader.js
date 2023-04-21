93edc16dee6a1f57afdbed6e47ec7fa85e23f0d8
Richard Talens <dicktalens@Richards-MacBook-Pro.local>
2023-04-21 15:20:01 -0500

    add fb pixel

--- website/gtm_loader.js
+++ website/gtm_loader.js
@@ -9,3 +9,14 @@ console.log('gtm loader activated');
     j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
     f.parentNode.insertBefore(j, f);
   })(window, document, "script", "dataLayer", "GTM-K5N4Z5F");
+
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '771141357683128');
+fbq('track', 'PageView');
